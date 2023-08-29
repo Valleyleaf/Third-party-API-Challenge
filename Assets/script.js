2e7cb9ed2416dbda0e888a1a65039df1ef804131
Aston Dahllöf <Aston_dahllof@outlook.com>
2023-08-28 22:24:23 -0300

    Finished the getlogs function to display all saved items. Also added addition ID's to index to make sure each item is saved at the proper hour mark.

--- Assets/script.js
+++ Assets/script.js
@@ -1,86 +1,31 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
-$(document).ready(function () {
-  // TODO: Add a listener for click events on the save button. This code should
-  // use the id in the containing time-block as a key to save the user input in
-  // local storage. HINT: What does `this` reference in the click listener
-  // function? How can DOM traversal be used to get the "hour-x" id of the
-  // time-block containing the button that was clicked? How might the id be
-  // useful when saving the description in local storage?
-  //
-  // TODO: Add code to apply the past, present, or future class to each time
-  // block by comparing the id to the current hour. HINTS: How can the id
-  // attribute of each time-block be used to conditionally add or remove the
-  // past, present, and future classes? How can Day.js be used to get the
-  // current hour in 24-hour time?
-  //
-  // TODO: Add code to get any user input that was saved in localStorage and set
-  // the values of the corresponding textarea elements. HINT: How can the id
-  // attribute of each time-block be used to do this?
-  //
-  // TODO: Add code to display the current date in the header of the page. COMPLETE
 
-//  ```md
-//GIVEN I am using a daily planner to create a schedule
-//WHEN I open the planner COMPLETE
-//THEN the current day is displayed at the top of the calendar COMPLETE
-//WHEN I scroll down
-//THEN I am presented with timeblocks for standard business hours of 9am&ndash;5pm
-//WHEN I view the timeblocks for that day
-//THEN each timeblock is color coded to indicate whether it is in the past, present, or future
-//WHEN I click into a timeblock
-//THEN I can enter an event
-//WHEN I click the save button for that timeblock
-//THEN the text for that event is saved in local storage
-//WHEN I refresh the page
-//THEN the saved events persist
-//```
+$(document).ready(function () {
 
 // VARIABLES AND CALLS GO HERE.
 var currentDate = dayjs();
 console.log('DAYJS IS: ' + currentDate)
 var today = currentDate.day();
 var date = dayjs().get('date');
-
 var currentHour = dayjs().get('hour')
-
 var dayVisual = document.getElementById('dayVisual')
 var dateFormat = currentDate.format('DD-MM-YYYY');
-console.log('Current dayJS time is: ' + dateFormat);
 var weekVariable = ['Sunday','Monday','Tuesday','Wednesday','Thursday','Friday','Saturday','Sunday'];
-
-console.log('Day is: ' + weekVariable[today]);
-console.log('Date is: ' + date);
 dayVisual.textContent = weekVariable[today] + ': ' + dateFormat;
-
+userSchedualeItem = []
 hoursArray = []
 for (let i = 9; i <= 17; i++) {
   hoursArray.push({name: `hour-${i}`, hourX: i});
+  userSchedualeItem.push(`log-${i}`);
   // Stumbled across this video via a StackOverflow article: https://www.youtube.com/watch?v=jO4TNhNNeYs
   // I would highly recommend adding ` to the curriculum as it seems to be a very useful feature.
   // console.log(hoursArray[i - 9].name)
   // console.log(hoursArray[i - 9].hourX)
+  // console.log(userSchedualeItem[i - 9])
 }
-
-// hourTextArea = []
-// function addTextAreaIDs(){
-//     for (let i = 0; i < hoursArray.length; i++){
-//       var textarea= document.getElementsByClassName(textarea);
-//       var hourTextArea = document.getElementsByClassName(textarea + i);
-//       console.log(hourTextArea + i);
-//       console.log(textarea);
-//     }
-// }
-
-// addTextAreaIDs();
-
-// Create a function that will check for the current hour. If lesser, make red, if equal, make yellow. If greater, make green.
 
   function applyTimeColor(){
     for (let i = 0; i < hoursArray.length; i++){
       var dayColor = document.getElementById(hoursArray[i].name);
-      //The above creates a VAR which value will be 'hour-Y'.
       // console.log('AAA: ' + hoursArray[i].name)
       if (hoursArray[i].hourX < currentHour){
         //PAST
@@ -101,19 +46,19 @@ for (let i = 9; i <= 17; i++) {
   applyTimeColor();
 // Save in local storage function.
 
-userSchedualeItem = []
-function repeatUserInputArray(userInput){
-  for (let i = 0; i < hoursArray.length; i++){
-    //This needs to check every item in the hoursArray, declare it's ID and then match it to the nested div "textcontent".
-    userSchedualeItem.push({name: `hour-${i}`, userSchedualeItem: i});
-    localStorage.setItem("log", JSON.stringify(userSchedualeItem));
-    console.log("button clicked");
-    console.log(userSchedualeItem[i]);
-    //Note, the above triggers 9 times.
-  }
-
+function getlogs(){
+  $('#hour-9 .description').val(localStorage.getItem('namehour-9'));
+  $('#hour-10 .description').val(localStorage.getItem('namehour-10'));
+  $('#hour-11 .description').val(localStorage.getItem('namehour-11'));
+  $('#hour-12 .description').val(localStorage.getItem('namehour-12'));
+  $('#hour-13 .description').val(localStorage.getItem('namehour-13'));
+  $('#hour-14 .description').val(localStorage.getItem('namehour-14'));
+  $('#hour-15 .description').val(localStorage.getItem('namehour-15'));
+  $('#hour-16 .description').val(localStorage.getItem('namehour-16'));
+  $('#hour-17 .description').val(localStorage.getItem('namehour-17'));
 }
 
+getlogs();
 
 // Logic will work as follows. There will be a div in the HTML for each hour between
 // 9-5. I will then implement a Javascript/jquery function that will check on the
@@ -127,11 +72,21 @@ function repeatUserInputArray(userInput){
 //.hide .hover .click .dblclick .mouseout are some examples. Also look up the "This" stuff cause that means something. 
 //Does it mean, do "this"? Also every line of code within a {} needs a ;
 $(document).ready(function(){
-  $('.save-button').click(function(event) {
-    repeatUserInputArray();
-    event.preventDefault();
+  $('.save-button').click(function(event){
+    console.log('this: ' + this)
+    let parent = $(this).parent();
+    // The above applies logic to all parents (button) remember this.
+    let logValue = parent.find('.description').val();
+    let timeValue = parent.attr('id');
+    console.log('logValue:' + logValue);
+    console.log('timeValue: ' + timeValue);
+    let uniqueNameKeyValue = 'name' + timeValue;
+    localStorage.setItem(uniqueNameKeyValue, logValue);
+    localStorage.setItem('clock', timeValue);
   });
 })
+
+// The above does not pass any data. Fix.
 
 });
 //The function above ensures that this is loaded before the website gets rendered.
